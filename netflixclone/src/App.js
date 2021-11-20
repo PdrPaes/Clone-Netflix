87f19a7b35a83548de8526ac8255b7fb4749215a
Pedro Henrique Paes <phpaes252@gmail.com>
2021-11-20 02:07:21 -0300

    :rocket:

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Tmdb from './Tmdb';
 
 export default () => {
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      // Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      console.log(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <div>  
-      Hello World no React! 
-    </div>
-  );
+    <div>Hello World no React! </div>
+  )
 }
--- netflixclone/src/Tmdb.js
+++ netflixclone/src/Tmdb.js
@@ -12,50 +12,58 @@ const API_BASE = 'https://api.themoviedb.org/3';
 - aventura
 */
 
-const basicFetch = async(endpoint) =>{
+const basicFetch = async (endpoint) => {
     const req = await fetch(`${API_BASE}${endpoint}`);
     const json = await req.json();
     return json;
 }
 
 
+
 export default {
-    getHomeList: async() =>{
+    getHomeList: async () => {
         return [
             {
                 slug: 'originals',
                 title: 'Originais da Netflix',
-                items: await basicFetch(`/discover/tv `)
+                items: await basicFetch(`/discover/tv?with_network=213language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'trending',
-                title: 'Recomendados para Você',
-                items: []
+                title: 'Recomendados para você',
+                items: await basicFetch(`/trending/all/week?language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'toprated',
-                title: 'Em Alta',
-                items: []
+                title: 'Em alta',
+                items: await basicFetch(`/movie/top_rated?language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=28&language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=35&language=en-US&api_key=${API_KEY}`)
             },
             {
-                slug: 'documentary',
-                title: 'Documentário',
-                items: []
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=en-US&api_key=${API_KEY}`)
+
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=en-US&api_key=${API_KEY}`)
+
             },
         ];
     }
